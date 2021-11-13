26d9497d172a9abde5c2cc5b143ed5a81990a174
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-13 13:35:19 +0700

    Update working.h

--- 0b/working.h
+++ 0b/working.h
@@ -11,5 +11,5 @@
 
 bool comp(std::pair<int, std::string> a, std::pair<int, std::string> b);
 
-std::map <std::string, int> count_words(const std::ifstream& in);
+std::map <std::string, int> count_words(std::ifstream& in);
 void print(std::map <std::string, int>& dict, std::ofstream& out);
