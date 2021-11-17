d70c810b539c2cdf6f999a0f5bbbb0949096cdb1
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-17 20:19:59 +0800

    Update trit.cpp

--- 1/trit.cpp
+++ 1/trit.cpp
@@ -6,19 +6,19 @@ namespace TRIT {
 		value = tv;
 	}
 
-	Trit Trit::operator&(Trit t1, Trit t2) {
+	Trit operator&(const Trit& t1,const Trit& t2) {
 		if (t1.value == False || t2.value == False) return Trit(False);
 		if (t1.value == Unknown || t2.value == Unknown) return Trit();
 		return Trit(True);
 	}
 
-	Trit Trit::operator|(Trit t1, Trit t2){
+	Trit operator|(const Trit& t1,const Trit& t2){
 		if (t1.value == True || t2.value == True) return Trit(True);
 		if (t1.value == Unknown || t2.value == Unknown) return Trit();
 		return Trit(False);
 	}
 
-	Trit Trit::operator~(Trit t){
+	Trit Trit::operator~(const Trit& t){
 		if (t.value == Unknown) return Trit();
 		if (t.value == False) return Trit(True);
 		return Trit(False);
