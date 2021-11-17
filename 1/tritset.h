5e1ea4849159635c08c5c94d5963f9689deb1874
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-17 20:18:36 +0800

    Update tritset.h

--- 1/tritset.h
+++ 1/tritset.h
@@ -6,21 +6,47 @@
 
 namespace TRIT{
 
-	class Tritset {
-	private:
-		std::vector<uint> data;
-		size_t elem_size;
-		size_t elem_capacity;
+	class TritPlacer;
 
-	public:
-		Tritset(size_t size);
-		size_t capacity();
-		size_t num_unident();
-		void shrink();
-		size_t cardinality(Trit_value tv);// ещё не умею
+	class TritSet {
+		private:
+			std::vector<uint> data;
+			size_t elem_size; //размер элем вектора
+			size_t elem_capacity; //число тритов в элем вектора
+
+			size_t num_unident_elem(); //число неустановл элем
+			size_t num_unident_trit(); //число неустановл тритов
 		
+		public:
+			TritSet(size_t size);
+			size_t capacity(); //число тритов в data
+			void shrink(); //освобождение пустой памяти
+			size_t cardinality(Trit_value tv); //число tv тритов в data
+			std::unordered_map<TritValue, size_t> cardinality(); //число тритов каждого значения
+			size_t length(); //индекс последнего не Unknown трита +1
+			void trim(size_t lastIndex); //забыть содержимое от lastIndex и дальше
+
+			TritSet operator~(const TritSet& T);
 		
+		friend class TritPlacer;
+		friend TritSet operator&(const TritSet& T1,const TritSet& T2);
+		friend TritSet operator|(const TritSet& T1,const TritSet& T2);
 	};
+
+	TritSet operator&(const TritSet& T1,const TritSet& T2);
+	TritSet operator|(const TritSet& T1,const TritSet& T2);
+
+	class TritPlacer {//////////////FIXME!!!!
+		private:
+			TritSet* tsp;
+			int index;
+
+		public:
+			TritPlacer(TritSet* ptr, int ind);
+			operator TritValue();
+
+	};
+
 
 
 }
