c87496fbeb7d65fa8610a625030bd453c5497a59
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-20 14:15:15 +0700

    Update trit.h

--- 1/trit.h
+++ 1/trit.h
@@ -5,23 +5,25 @@
 namespace TRIT {
 
 	enum Trit_value {
-		Unknown = 0, 
-		False = 1, 
-		True = 3    
+		Unknown = 0,
+		False = 1,
+		True = 3
 	};
 
 	class Trit {
 		Trit_value value;
+	public:
+		
+		Trit(Trit_value tv);
 
-		public:
-			Trit(Trit_value tv);
+		Trit operator~();
+		bool operator==(Trit_value tv);
+		void operator=(Trit_value tv);
 
-			Trit operator~(const Trit& t);
-
-		friend Trit operator&(const Trit& t1,const Trit& t2);
-		friend Trit operator|(const Trit& t1,const Trit& t2);
+		friend Trit operator&(const Trit& t1, const Trit& t2);
+		friend Trit operator|(const Trit& t1, const Trit& t2);
 	};
 
-	Trit operator&(const Trit& t1,const Trit& t2);
-	Trit operator|(const Trit& t1,const Trit& t2);
+	Trit operator&(const Trit& t1, const Trit& t2);
+	Trit operator|(const Trit& t1, const Trit& t2);
 }
