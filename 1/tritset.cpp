cb90e2e1c1ec9306b8507a10388c9e646dc9b1ce
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-20 14:14:56 +0700

    Update tritset.cpp

--- 1/tritset.cpp
+++ 1/tritset.cpp
@@ -3,9 +3,9 @@
 #include "trit.h"
 #include "tritset.h"
 
-namespace TRIT{
-	Tritset::TritSet(size_t size = 0){
-		elem_size = sizeof(uint);
+namespace TRIT {
+	TritSet::TritSet(size_t size = 0) {
+		elem_size = sizeof(unsigned int);
 		elem_capacity = 4 * elem_size;
 		size_t num_elem = size / elem_capacity;
 		if (size && size % elem_capacity)
@@ -13,27 +13,27 @@ namespace TRIT{
 		data.resize(num_elem, 0);
 	}
 
-	size_t TritSet::capacity(){
+	size_t TritSet::capacity() {
 		return data.size() * elem_capacity;
 	}
 
-	size_t TritSet::num_unident_elem(){
+	size_t TritSet::num_unident_elem() {
 		size_t num = 0;
 		for (int i = data.size() - 1; i >= 0 && !data[i]; i--, num++);
 		return num;
 	}
 
-	size_t TritSet::num_unident_trit(){
+	size_t TritSet::num_unident_trit() {
 		size_t num = 0;
 		for (int i = capacity() - 1; i >= 0 && (*this)[i] == Unknown; i--, num++);
 		return num;
 	}
 
-	void TritSet::shrink(){
+	void TritSet::shrink() {
 		data.resize(data.size() - num_unident_elem());
 	}
 
-	size_t TritSet::cardinality(Trit_value tv){
+	size_t TritSet::cardinality(Trit_value tv) {
 		size_t ans = 0;
 		size_t n = length();
 		for (int i = 0; i < n; i++)
@@ -42,120 +42,130 @@ namespace TRIT{
 		return ans;
 	}
 
-	std::unordered_map<TritValue, size_t> TritSet::cardinality(){
-		std::unordered_map<TritValue, size_t> res;
+	std::unordered_map<Trit_value, size_t> TritSet::cardinality() {
+		std::unordered_map<Trit_value, size_t> res;
 		size_t n = length();
 		for (int i = 0; i < n; i++)
 			res[(*this)[i]]++;
 		return res;
 	}
 
-	size_t TritSet::length(){
+	size_t TritSet::length() {
 		return capacity() - num_unident_trit();
 	}
 
-	void TritSet::trim(size_t lastIndex){
+	void TritSet::trim(size_t lastIndex) {
 		if (lastIndex < capacity())
 			data.resize(lastIndex);
 		return;
 	}
 
-	TritSet TritSet::operator~(const TritSet& T){
+	TritSet TritSet::operator~() {
 		size_t n = capacity();
 		TritSet res(n);
-		for (int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			Trit t = (*this)[i];
 			res[i] = ~t;
 		}
 		return res;
 	}
 
-	TritPlacer TritSet::operator[](int index){
+	TritPlacer TritSet::operator[](int index) {
 		return TritPlacer(this, index);
 	}
 
-	TritSet operator&(const TritSet& T1,const TritSet& T2){
+	TritSet operator&(const TritSet& T1, const TritSet& T2) {
 		size_t len1 = T1.capacity();
 		size_t len2 = T2.capacity();
 		size_t n = std::max(len1, len2);
 		TritSet res(n);
-		for (int i = 0; i < n; i++){
-			Trit t1, t2;
+		for (int i = 0; i < n; i++) {
+			Trit t1(), t2();
 			if (i < len1) t1 = T1[i];
 			if (i < len2) t2 = T2[i];
 			res[i] = t1 & t2;
 		}
 		return res;
 	}
-	TritSet operator|(const TritSet& T1,const TritSet& T2){
+	TritSet operator|(TritSet& T1, TritSet& T2) {
 		size_t len1 = T1.capacity();
 		size_t len2 = T2.capacity();
 		size_t n = std::max(len1, len2);
 		TritSet res(n);
-		for (int i = 0; i < n; i++){
-			Trit t1, t2;
-			if (i < len1) t1 = T1[i];
-			if (i < len2) t2 = T2[i];
-			res[i] = t1 | t2;
+		for (int i = 0; i < n; i++) {
+			Trit t1(), t2();
+			if (i < len1 && i < len2) {
+				Trit t1(T1[i]), t2(T2[i]);
+				res[i] = t1 | t2;
+			}
+			else if (i < len1) {
+				Trit t1(T1[i]);
+				Trit t2();
+				res[i] = t1 | t2;
+			}
+			else if (i < len2) {
+				Trit t1(), t2(T2[i]);
+				res[i] = t1 | t2;
+			}
 		}
 		return res;
 	}
 
 
-	TritPlacer::TritPlacer(TritSet* ptr, int ind){
+	TritPlacer::TritPlacer(TritSet* ptr, int ind) {
 		tsp = ptr;
 		index = ind;
 	}
 
-	TritPlacer::operator TritValue(){
+	TritPlacer::operator Trit_value() {
 		size_t ind = index / tsp->elem_capacity;
 		size_t pos = index % tsp->elem_capacity;
-		uint shift = 2 * (tsp->elem_capacity - pos - 1);
-		uint mask = 3 << shift;
-		uint tv = (tsp->data[ind] & mask) >> shift;
+		unsigned int shift = 2 * (tsp->elem_capacity - pos - 1);
+		unsigned int mask = 3 << shift;
+		unsigned int tv = (tsp->data[ind] & mask) >> shift;
 
 		if (!tv) return Unknown;
 		if (tv == 1) return False;
 		if (tv == 3) return True;
 	}
 
-	TritPlacer::operator Trit(){
-		return Trit(static_cast<TritValue>(*this));
+	TritPlacer::operator Trit() {
+		return Trit(static_cast<Trit_value>(*this));
 	}
 
-	void TritPlacer::operator=(TritValue tv){
+	void TritPlacer::operator=(Trit_value tv) {
 		this->operator=(Trit(tv));
 	}
 
-	void TritPlacer::operator=(Trit t){
+	void TritPlacer::operator=(Trit t) {
 		size_t ind = index / tsp->elem_capacity;
 		size_t pos = index % tsp->elem_capacity;
-		uint shift = 2 * (tsp->elem_capacity - pos - 1);
-		if (t == Unknown){
-			if (index < tsp->capacity()){
-				uint mask = ~(3 << shift);
+		unsigned int shift = 2 * (tsp->elem_capacity - pos - 1);
+		if (t == Unknown) {
+			if (index < tsp->capacity()) {
+				unsigned int mask = ~(3 << shift);
 				tsp->data[ind] &= mask;
 			} return;
 		}
-		if (index >= tsp->capacity()){
+		if (index >= tsp->capacity()) {
 			size_t new_size = index / tsp->elem_capacity;
 			if (index % tsp->elem_capacity)
 				new_size++;
 			tsp->data.resize(new_size, 0);
 		}
-		if (t == True){
-			uint mask = 3 << shift;
+		if (t == True) {
+			unsigned int mask = 3 << shift;
 			tsp->data[ind] |= mask;
 		}
-		if (t == False){
-			uint mask = ~(3 << shift);
+		if (t == False) {
+			unsigned int mask = ~(3 << shift);
 			tsp->data[ind] &= mask;
 			mask = 1 << shift;
 			tsp->data[ind] |= mask;
 		}
 	}
 
-	void TritPlacer::operator=(TritPlacer tp){
+	void TritPlacer::operator=(TritPlacer tp) {
 		Trit t = tp;
 		*this = t;
 	}
