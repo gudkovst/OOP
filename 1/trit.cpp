41c0e69644201a0fb9babe9f640e8493deef0cc6
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-11-27 13:39:49 +0700

    Update trit.cpp

--- 1/trit.cpp
+++ 1/trit.cpp
@@ -1,29 +1,27 @@
 #include "trit.h"
 
-namespace TRIT {
-	Trit::Trit(Trit_value tv = Unknown) {
-		value = tv;
-	}
+Trit::Trit(Trit_value tv = Unknown) {
+	value = tv;
+}
 
-	bool Trit::operator==(Trit_value tv) {
-		return (value == tv);
-	}
+bool Trit::operator==(Trit_value tv) {
+	return (value == tv);
+}
 
-	Trit Trit::operator~() {
-		if (this->value == Unknown) return Trit();
-		if (this->value == False) return Trit(True);
-		return Trit(False);
-	}
+Trit Trit::operator~() {
+	if (this->value == Unknown) return Trit();
+	if (this->value == False) return Trit(True);
+	return Trit(False);
+}
 
-	Trit operator&(const Trit& t1, const Trit& t2) {
-		if (t1.value == False || t2.value == False) return Trit(False);
-		if (t1.value == Unknown || t2.value == Unknown) return Trit();
-		return Trit(True);
-	}
+Trit operator&(const Trit& t1, const Trit& t2) {
+	if (t1.value == False || t2.value == False) return Trit(False);
+	if (t1.value == Unknown || t2.value == Unknown) return Trit();
+	return Trit(True);
+}
 
-	Trit operator|(const Trit& t1, const Trit& t2) {
-		if (t1.value == True || t2.value == True) return Trit(True);
-		if (t1.value == Unknown || t2.value == Unknown) return Trit();
-		return Trit(False);
-	}
+Trit operator|(const Trit& t1, const Trit& t2) {
+	if (t1.value == True || t2.value == True) return Trit(True);
+	if (t1.value == Unknown || t2.value == Unknown) return Trit();
+	return Trit(False);
 }
