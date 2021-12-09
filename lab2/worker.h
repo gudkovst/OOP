d650ac9ceaf146bcdcb43828ecf6a580756cdaca
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-09 16:03:03 +0700

    Update worker.h

--- lab2/worker.h
+++ lab2/worker.h
@@ -7,59 +7,60 @@
 #include <regex>
 #include <algorithm>
 
+#include "exceptions.h"
 
-class Worker{
-private:
+class Worker {
+protected:
 	bool input;
 	bool output;
-	public:
-		virtual void execute(std::vector<std::string>& text) = 0;
-		bool isInput();
-		bool isOutput();
+public:
+	virtual void execute(std::vector<std::string>& text) = 0;
+	bool isInput();
+	bool isOutput();
 };
 
-class WorkerRead: public Worker{
-	private:
-		std::string filename;
-	public:
-		WorkerRead(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerRead : public Worker {
+private:
+	std::string filename;
+public:
+	WorkerRead(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
 
-class WorkerWrite: public Worker{
-	private:
-		std::string filename;
-	public:
-		WorkerWrite(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerWrite : public Worker {
+private:
+	std::string filename;
+public:
+	WorkerWrite(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
 
-class WorkerGrep: public Worker{
-	private:
-		std::string word;
-	public:
-		WorkerGrep(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerGrep : public Worker {
+private:
+	std::string word;
+public:
+	WorkerGrep(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
 
-class WorkerSort: public Worker{
-	public:
-		WorkerSort(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerSort : public Worker {
+public:
+	WorkerSort(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
 
-class WorkerReplace: public Worker{
-	private:
-		std::string word1, word2;
-	public:
-		WorkerReplace(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerReplace : public Worker {
+private:
+	std::string word1, word2;
+public:
+	WorkerReplace(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
 
-class WorkerDump: public Worker{
-	private:
-		std::string filename;
-	public:
-		WorkerDump(const std::string& argv);
-		void execute(std::vector<std::string>& text) override;
+class WorkerDump : public Worker {
+private:
+	std::string filename;
+public:
+	WorkerDump(const std::string& argv);
+	void execute(std::vector<std::string>& text) override;
 };
