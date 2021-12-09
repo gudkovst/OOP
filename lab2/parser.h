f70b140daa40c54c3ae2bddacc5a0c3e654165e8
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-09 16:02:36 +0700

    Update parser.h

--- lab2/parser.h
+++ lab2/parser.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,16 +8,17 @@
 
 #include "factory.h"
 #include "worker.h"
+#include "exceptions.h"
 
-enum ParserState{
-	dream, open, close, end 
+enum ParserState {
+    dream, open, close, end
 };
 
-class Parser{
+class Parser {
 private:
-	std::ifstream fin;
-	std::string buffer;
-	std::vector<int> commandSequence;
+    std::ifstream fin;
+    std::string buffer;
+    std::vector<int> commandSequence;
     std::map<int, Worker*> blocks;
     WorkerReadFactory* ReadFactory;
     WorkerWriteFactory* WriteFactory;
@@ -28,9 +31,9 @@ private:
     std::vector<int> parseSequence(const std::string& buffer);
 
 public:
-	Parser(const std::string& filename);
-	void parse();
+    Parser(const std::string& filename);
+    void parse();
 
-	std::vector<int> getSequence();
-	std::map<int, Worker*> getBlocks();
+    std::vector<int> getSequence();
+    std::map<int, Worker*> getBlocks();
 };
