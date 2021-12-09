1371d10cf7a8c4fe177474335626b52ca3f8bcc9
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-09 13:43:01 +0800

    Create exceptions.h

---
+++ lab2/exceptions.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <stdexcept>
+
+class TooMushArgs: public std::exception{
+private:
+	std::string meserr;
+public:
+	TooMushArgs(const std::string& message);
+	const char* what() const noexcept override; 
+};
+
+class TooFewArgs: public std::exception{
+private:
+	std::string meserr;
+public:
+	TooFewArgs(const std::string& message);
+	const char* what() const noexcept override; 
+};
+
+class FileBeginningError: public std::exception{
+private:
+	std::string meserr;
+public:
+	FileBeginningError();
+	const char* what() const noexcept override;
+};
+
+class UnknownWorker: public std::exception{
+private:
+	std::string meserr;
+public:
+	UnknownWorker();
+	const char* what() const noexcept override;
+};
+
+class DelimiterError: public std::exception{
+private:
+	std::string meserr;
+public:
+	DelimiterError();
+	const char* what() const noexcept override;
+};
+
+class DescriptionBlockError: public std::exception{
+private:
+	std::string meserr;
+public:
+	DescriptionBlockError();
+	const char* what() const noexcept override;
+}
+
+class DescriptionSequenceError: public std::exception{
+private:
+	std::string meserr;
+public:
+	DescriptionSequenceError();
+	const char* what() const noexcept override;
+}
+
