9277bd8295240f887fe4f9b34828cf33ae803968
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-09 13:44:36 +0800

    Create parser.h

---
+++ lab2/parser.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "factory.h"
+#include "worker.h"
+
+enum ParserState{
+	dream, open, close, end 
+};
+
+class Parser{
+private:
+	std::ifstream fin;
+	std::string buffer;
+	std::vector<int> commandSequence;
+    std::map<int, Worker*> blocks;
+    WorkerReadFactory* ReadFactory;
+    WorkerWriteFactory* WriteFactory;
+    WorkerGrepFactory* GrepFactory;
+    WorkerSortFactory* SortFactory;
+    WorkerReplaceFactory* ReplaceFactory;
+    WorkerDumpFactory* DumpFactory;
+
+    std::vector<std::string> parseBlock(const std::string& buffer);
+    std::vector<int> parseSequence(const std::string& buffer);
+
+public:
+	Parser(const std::string& filename);
+	void parse();
+
+	std::vector<int> getSequence();
+	std::map<int, Worker*> getBlocks();
+};
