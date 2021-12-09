f43fab508503ef98963b3419acd5e5d745bdec36
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-09 13:44:13 +0800

    Create factory.cpp

---
+++ lab2/factory.cpp
@@ -0,0 +1,30 @@
+#include "factory.h"
+
+
+Worker* WorkerReadFactory::createWorker(std::string argv){
+	return new WorkerRead(argv);
+}
+
+Worker* WorkerWriteFactory::createWorker(std::string argv){
+	return new WorkerWrite(argv);
+}
+
+Worker* WorkerGrepFactory::createWorker(std::string argv){
+	return new WorkerGrep(argv);
+}
+
+Worker* WorkerSortFactory::createWorker(std::string argv){
+	return new WorkerSort(argv);
+}
+
+Worker* WorkerReplaceFactory::createWorker(std::string argv){
+	return new WorkerReplace(argv);
+}
+
+Worker* WorkerDumpFactory::createWorker(std::string argv){
+	return new WorkerDump(argv);
+}
+
+Worker* makeWorker(WorkerFactory* factory, std::string& argv){
+	return factory->createWorker(argv);
+}
