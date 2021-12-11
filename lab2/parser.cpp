9c01971a66a9e189a19eb8f87da8cf3cf9c39f62
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-12-11 18:02:27 +0800

    Update parser.cpp

--- lab2/parser.cpp
+++ lab2/parser.cpp
@@ -9,7 +9,112 @@ Parser::Parser(const std::string& filename) {
 	ReplaceFactory = new WorkerReplaceFactory;
 	DumpFactory = new WorkerDumpFactory;
 }
+#include "parser.h"
 
+Parser::Parser(const std::string& filename) {
+	fin.open(filename);
+	if (!fin) throw FileBeginningError();
+	ReadFactory = new WorkerReadFactory;
+	WriteFactory = new WorkerWriteFactory;
+	GrepFactory = new WorkerGrepFactory;
+	SortFactory = new WorkerSortFactory;
+	ReplaceFactory = new WorkerReplaceFactory;
+	DumpFactory = new WorkerDumpFactory;
+}
+
+std::vector<int> Parser::getSequence() {
+	return commandSequence;
+}
+
+std::map<int, Worker*> Parser::getBlocks() {
+	return blocks;
+}
+
+std::vector<std::string> Parser::parseBlock(const std::string& buffer) {
+	std::vector<std::string> res(3);
+	int i = 0;
+	for (; buffer[i] == ' ' && i < buffer.size(); i++);
+	if (i == buffer.size()) throw DescriptionBlockError("1");
+	res[0].clear();
+	for (; std::isdigit(buffer[i]) && i < buffer.size(); i++)
+		res[0] += buffer[i];
+	if (i < buffer.size() - 3 && buffer[i] == ' ' && buffer[i + 1] == '='
+		&& buffer[i + 2] == ' ') i += 3;
+	else throw DescriptionBlockError("2");
+	res[1].clear();
+	for (; buffer[i] != ' ' && i < buffer.size(); i++)
+		res[1] += buffer[i];
+	std::cout << res[1] << '\n';
+	for (; buffer[i] == ' ' && i < buffer.size(); i++);
+	res[2] = buffer.substr(i);
+	return res;
+}
+
+std::vector<int> Parser::parseSequence(const std::string& buffer) {
+	std::vector<int> res;
+	int i = 0;
+	bool prevOut = false;
+	for (; buffer[i] == ' ' && i < buffer.size(); i++);
+	if (i == buffer.size()) throw DescriptionSequenceError();
+	while (i < buffer.size()) {
+		if (!std::isdigit(buffer[i])) throw DescriptionSequenceError();
+		int blockNum = 0;
+		for (; std::isdigit(buffer[i]) && i < buffer.size(); i++)
+			blockNum = blockNum * 10 + buffer[i] - '0';
+		if (blocks[blockNum]->isInput() == prevOut)
+			prevOut = blocks[blockNum]->isOutput();
+		else throw DescriptionSequenceError();
+		res.push_back(blockNum);
+		if (i < buffer.size() - 4 && buffer[i] == ' ' && buffer[i + 1] == '-'
+			&& buffer[i + 2] == '>' && buffer[i + 3] == ' ') i += 4;
+		else throw DescriptionSequenceError();
+	}
+	return res;
+}
+
+void Parser::parse() {
+	ParserState state = dream;
+	while (!fin.eof()) {
+		std::getline(fin, buffer);
+		std::cout << buffer << '\n';
+		if (state == dream) {
+			if (buffer.substr(0,4) == "desc") {
+				state = open;
+				continue;
+			}
+			else throw FileBeginningError();
+		}
+		if (state == open) {
+			if (buffer == "csed") {
+				state = close;
+				continue;
+			}
+			else {
+				std::vector<std::string> block = parseBlock(buffer);
+				int number = std::stoi(block[0]);
+				if (block[1].substr(0, 8) == "readfile")
+					blocks[number] = makeWorker(ReadFactory, block[2]);
+				if (block[1] == "writefile")
+					blocks[number] = makeWorker(WriteFactory, block[2]);
+				if (block[1] == "grep")
+					blocks[number] = makeWorker(GrepFactory, block[2]);
+				if (block[1] == "sort")
+					blocks[number] = makeWorker(SortFactory, block[2]);
+				if (block[1] == "replace")
+					blocks[number] = makeWorker(ReplaceFactory, block[2]);
+				if (block[1] == "dump")
+					blocks[number] = makeWorker(DumpFactory, block[2]);
+				else throw UnknownWorker();
+			}
+		}
+		if (state == close) {
+			state = end;
+			commandSequence = parseSequence(buffer);
+		}
+	}
+	if (state != end) throw DelimiterError();
+	fin.close();
+}
 std::vector<int> Parser::getSequence() {
 	return commandSequence;
 }
