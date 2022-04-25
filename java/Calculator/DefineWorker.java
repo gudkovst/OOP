9a63a8b2a1c8e2a18b101c699af910c9a96e20a1
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-04-25 20:17:21 +0700

    Add files via upload

---
+++ java/Calculator/DefineWorker.java
@@ -0,0 +1,37 @@
+package workers;
+
+import exceptions.WorkerException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class DefineWorker implements Worker{
+    ArrayList<String> args;
+    public DefineWorker(ArrayList<String> newArgs){
+        args = newArgs;
+    }
+    public DefineWorker(){}
+
+    public void setArgs(ArrayList<String> args) {
+        this.args = args;
+    }
+
+    public void execute(Stack<Double> stack, HashMap<String, Double> varBase) throws WorkerException {
+        if (args.size() != 2)
+            throw new WorkerException(this.getClass().getName(), "Invalid number of parameters:" + args.size());
+        String name = args.get(0);
+        String strValue = args.get(1);
+        Double fValue;
+        try {
+            fValue = Double.parseDouble(strValue);
+        }
+        catch (NumberFormatException nfe){
+            if (varBase.containsKey(strValue))
+                fValue = varBase.get(strValue);
+            else
+                throw new WorkerException(this.getClass().getName(), "Wrong parameter entry");
+        }
+        varBase.put(name, fValue);
+    }
+}
